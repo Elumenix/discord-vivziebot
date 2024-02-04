65b76ed2516377962cc1e0f592debe21eb75913d
Elumenix <dps5393@g.rit.edu>
2024-02-04 14:50:18 -0500

    previous change appeared to prevent login process, so activating server connection after that

--- src/discord/deploy.ts
+++ src/discord/deploy.ts
@@ -1,7 +1,6 @@
 import { REST, Routes } from "discord.js";
 import dotenv from "../dotenv.js";
 import commands from "./commands/index.js";
-import * as http from 'http'
 
 await dotenv();
 
@@ -11,13 +10,3 @@ const rest = new REST({ version: "10" })
 
 await rest.put(Routes.applicationCommands(process.env.DISCORD_CLIENT_ID!), { body: commandData });
 console.log("Successfully deployed commands");
-
-const PORT = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-    // No server logic here
-});
-
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 import { login } from "./discord/index.js";
 import { sync } from "./models/index.js";
 import { pruneCooldowns } from "./prune.js";
+import * as http from 'http'
 
 try {
     await sync();
     await pruneCooldowns();
     await login();
+    
+    const PORT = process.env.PORT || 3000;
+
+    const server = http.createServer((req, res) => {
+        // No server logic here
+    });
+
+    server.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
 }
 catch (err) {
     console.error(err);
