2db9c25d20449dead783aa0ce63de365e0c84a52
Elumenix <dps5393@g.rit.edu>
2024-02-04 14:38:34 -0500

    Attempting to connect to Herokus port so it doesn't kill the program

--- src/discord/deploy.ts
+++ src/discord/deploy.ts
@@ -1,6 +1,7 @@
 import { REST, Routes } from "discord.js";
 import dotenv from "../dotenv.js";
 import commands from "./commands/index.js";
+const server = require('http');
 
 await dotenv();
 
@@ -10,3 +11,9 @@ const rest = new REST({ version: "10" })
 
 await rest.put(Routes.applicationCommands(process.env.DISCORD_CLIENT_ID!), { body: commandData });
 console.log("Successfully deployed commands");
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
